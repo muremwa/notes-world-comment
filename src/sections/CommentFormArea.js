242e5d8c2f310200414986d64a3410d1c1f8da82
Muremwa <danmburu254@gmail.com>
2021-06-08 17:15:16 +0300

    migrate form

--- src/sections/CommentFormArea.js
+++ src/sections/CommentFormArea.js
@@ -23,14 +23,20 @@ class CommentForm extends Component {
                     <textarea name='comment-textarea' className="form-control" cols="40" rows="10" maxLength="140" required id="id_comment" placeholder="add comment here (use '@username' to mention someone)"></textarea>
                 </div>
                 <hr />
-                <div className="alert alert-info" id="comment-alert-info" name='comment-info' style={hiddenFeature}>
-                    <button type="button" className="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    Posting your comment...
+
+                <div id="comment-alert-info" className="alert alert-info alert-dismissible fade show" role="alert" style={hiddenFeature}>
+                    <p>Posting your comment...</p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-                <div className="alert alert-warning" id="comment-alert-error" style={hiddenFeature}>
-                    Could not post your comment. Refresh the page and try again
+
+                <div id="comment-alert-error" className="alert alert-warning alert-dismissible fade show" role="alert" style={hiddenFeature}>
+                    <p>Could not post your comment. Refresh the page and try again</p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-                <button type='submit' className='col-sm-4 form-control'>Comment</button>
+
+                <div className="col-sm-4">
+                    <button type='submit' className='form-control'>Comment</button>
+                </div>
             </form>
         );
     };
