a828ec489b2e5abebc420cbc40f0a3d71f10b29e
Daniel Muremwa Mburu <daniel.muremwa@cellulant.io>
2023-01-08 10:51:52 +0300

    adds comments

--- src/sections/comments/Comment.js
+++ src/sections/comments/Comment.js
@@ -1,57 +1,66 @@
 import React from "react";
-import parseComment from '../../logic/CommentParse';
 
-import BottomAction from './bottom';
+import './Comment.css';
 
 
-function UserImage(props) {
-    /* 
+
+function UserImage({ profile, name }) {
+    /*
     user's image on the comment section
     */
     return (
-        <div className="col-sm-1 user-img">                
-            <img src={props.profile} alt={"profile picture for "+props.user} />
+        <div className="col-sm-1 r-user-img">
+            <img src={profile} alt={`profile picture for ${name}`} />
         </div>
     );
 }
 
+const Edited = ({ edited }) => edited? <span className='text-info'>edited sucker</span>: null;
 
-function Edited (props) {
-    if (!props.edited) {
-        return null;
-    }
-    return <span className="text-info">edited</span>;
-}
-
-
-function CommentBody (props) {
-    const { name, user, time, edited, replies, comment, commentId, ownsNote, editable, replyUrl, actionUrl } = props;
-    const cleanComment = parseComment(comment);
-
+function CommentBody ({ comment, note }) {
     return (
         <div className="col-sm-11">
-            <span className="comment-info">
-                <strong>{name} </strong> 
-                <small className="text-danger">@{user} </small> 
-                posted <strong className="dated">{time} </strong>
-                <Edited edited={edited} />
-                <span className="text-info"> {replies} replies</span>
+            <span className="r-comment-info">
+                <strong>{ comment.user.fullName }</strong>
+                <small className="text-danger">@{ comment.user.username }</small>
+                posted <strong className="dated">{ comment.time }</strong>
+                <Edited edited={comment.edited} />
+                <span className="text-info"> { comment.replies } {comment.replies === 1? 'reply': 'replies'}</span>
             </span>
-            <div className="comment-text" name='comment-text'>
-                <p dangerouslySetInnerHTML={{__html: cleanComment}}></p>
+
+            <div className="comment-text" id="comment-text">
+                <p>Comment text goes here</p>
             </div>
-            <BottomAction {...{commentId, ownsNote, comment, editable, replyUrl, actionUrl}} />
         </div>
-    );
-};
+    )
+}
 
 
-export default function Comment (props) {
+function Comment ({ comment, note }) {
     return (
-        <div className="row text-primary comment" id={`comment${props.commentId}`}>
-            <UserImage user={props.user} profile={props.imageUrl}/>
-            <CommentBody {...props}/>
+        <div className="row text-primary comment" id={`comment${ comment.commentId }`}>
+            <UserImage name={comment.user.username} profile={comment.user.profile}/>
+            <CommentBody {...{ comment, note }}/>
             <hr />
         </div>
     )
-};
+}
+
+
+export default function CommentSite ({ note }) {
+    if (note.comments.length === 0) {
+        return (
+            <div id="no-comments">
+                <h3 className="alert-heading text-center">no comments yet to { note.note }</h3>
+            </div>
+        );
+    }
+
+    const commentsParsed = note.comments.map((comment, k) => <Comment {...{ comment, note, key: k}} />);
+
+    return (
+        <div>
+            { commentsParsed }
+        </div>
+    )
+}
